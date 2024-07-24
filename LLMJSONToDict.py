906e2a1fe059c514e141a964967b9c70d306cf47
sudo-odner <odner.main@gmail.com>
2024-07-24 11:23:45 +0300

    add comment & stable result form custom_load

--- LLMJSONToDict.py
+++ LLMJSONToDict.py
@@ -2,43 +2,56 @@ class LLMJSONToDict:
     def __init__(self):
         self._cursor = 0
         self.text = ""
-        self._status_error = False
-        self._status_info = ""
+        self._error_status = False
+        self._error_info = ""
 
+    def new_text(self, text):
+        self._cursor = 0
+        self.text = text
+        self._error_status = False
+        self._error_info = ""
+
+    # Error if in recursion find error
     def _error(self, info: str):
-        self._status_error = True
-        self._status_info = info
+        self._error_status = True
+        self._error_info = info
 
-    # Get str and change cursor
-    def _add_parts(self, _local_idx: int):
-        _element_part = self.text[self._cursor+1:(self._cursor+_local_idx)]
-        self._cursor += _local_idx
-        _local_idx = 0
-        return _element_part
+    # Custom error
+    def _error_invalid_char(self, info: str):
+        self._error("""Invalid characters. Use single or dabble quotes for siting.""" + "Data:" + info)
 
+    def _error_invalid_dict(self):
+        self._error("""Length of data key is not equal to length of data value""")
+
+    # Filter key in JSON object.
+    # Key in JSON always is string, some time one word without single or dabble quotes.
+    # If text is string with single or dabble quotes -> add all body inside like key
+    # Else text without single or dabble quotes -> check space in name -> if True add key, else set Error
     def _filter_key(self, text: str) -> str:
         text = text.strip()
-        if (text[0] == "'" and text[-1] == "'") or (text[0] == '"' and text[-1] == '"'):
+        if (text[0] and text[-1] == "'") or (text[0] and text[-1] == '"'):
             return text[1:-1]
         else:
             if text.find(" ") != -1:
-                self._error("Invalid characters in key -> ' ' in line. Use single quotes.")
+                self._error_invalid_char(text)
             else:
                 return text
         return text
 
-    def _filter_value(self, text: str | list) -> list | dict | str | int | bool | None :
-        # If data in list is array, return array
-        if type(text) == list:
+    # Filter value
+    # Value in JSON have some type: array(list), object(dict), string, integer, boolean, null(None)
+    # This code convert string, integer, boolean, null(None)
+    # If we have str with zero rune -> return None
+    def _filter_value(self, text: str | list | dict) -> list | dict | str | int | bool | None :
+        # If text is array or dict, return array or dict
+        if (type(text) == list) or (type(text) == dict):
             return text
-        # If data in list is dict, return dict
-        elif type(text) == dict:
-            return text
-        # Else data in list is not array, select another type
+        # Else data is not array and dict, set type
         text = text.strip()
+        # If text is Zero return None
         if len(text) == 0:
             return None
-        elif (text[0] == "'" and text[-1] == "'") or (text[0] == '"' and text[-1] == '"'):
+        elif (text[0] and text[-1] == "'") or (text[0] and text[-1] == '"'):
             return text[1:-1]
         elif text.isnumeric():
             return int(text)
@@ -51,38 +64,54 @@ class LLMJSONToDict:
         elif text.lower().find("none") != -1:
             return None
 
-        self._error("Invalid characters in value -> ' ' in line. Use single quotes for string. Value: '{}'".format(text))
+        # If not found correct type -> set Error and return None.
+        self._error_invalid_char(text)
         return None
 
+    # Creating and filtering dict from array.
+    # Array created with pattern: first value -> key, second value -> value.
+    # Example: ["some", 1, "this is key", "this value"] -> {"some": 1, "this is key": "this value"}
+    # If we have different quantity key and value in array -> set Error.
     def _create_dict(self, data: list) -> dict:
-        data_key = data[::2]
-        data_value = data[1::2]
+        # Set key array and value array
+        data_key, data_value = data[::2], data[1::2]
+        # If array key and value have different quantity -> set Error.
         if len(data_key) != len(data_value):
-            self._error("Length of data key is not equal to length of data value")
+            self._error_invalid_dict()
             return dict()
 
         new_dict = dict()
         for idx in range(len(data_key)):
+            # Create a key and a value with a filtered type.
             name_key, name_val = self._filter_key(data_key[idx]), self._filter_value(data_value[idx])
-            if self._status_error:
+            # Check state Error, if we get an error -> exit the loop, else continue.
+            if self._error_status:
                 return dict()
             else:
                 new_dict[name_key] = name_val
 
         return new_dict
 
-
+    # Creating and filtering the received array
+    # If we have different quantity key and value in array -> set Error.
     def _create_array(self, data: list) -> list:
         new_array = list()
         for i in range(len(data)):
+            # Create a value with a filtered type.
             new_element = self._filter_value(data[i])
-            if self._status_error:
+            # Check state Error, if we get an error -> exit the loop, else continue.
+            if self._error_status:
                 return list()
             else:
                 new_array.append(new_element)
 
         return new_array
 
+    # Main function with recursion
+    # We have three types of trigger:
+    # 1. Deepening -> '{', '}', '[', ']'. They are necessary for deep recursion of the search for a certain type element.
+    # 2. Separation -> ',', ':', '}', ']'. They are necessary to perform the separation of elementsю
+    # 3. Contextual -> '/', '#', '\n', '"', "'". They needed for understand reading rune. In data string? This is comment?
     def next(self) -> list | dict:
         _last_rune = ""
 
@@ -106,34 +135,49 @@ class LLMJSONToDict:
             # If last rune is '['
             elif _last_rune == '[':
                 if rune == ':':
+                    # If rune have ':' in array, and it not in str context -> set Error
                     if not _context_str:
-                        self._error("Invalid characters in array -> array can't have ':'. Use single quotes.")
+                        self._error_invalid_char("Array can't have ':'")
+                        return list()
                 if rune == ',':
+                    # If rune have ',' in array - we create new part from self.context + 1 to self.context + _local_idx
+                    # After set _local_idx
+                    # if part is '', not append in parts
                     _element_part = self.text[self._cursor+1:(self._cursor+_local_idx)]
                     if _element_part.strip() != '':
                         parts.append(_element_part)
                     self._cursor += _local_idx
                     _local_idx = 0
                 elif rune == ']':
+                    # If rune have ']' in array - we create last part from self.context + 1 to self.context + _local_idx
+                    # After set _local_idx
+                    # if part is '', not append in parts
+                    # After return from recursion
                     _element_part = self.text[self._cursor+1:(self._cursor+_local_idx)]
                     parts.append(_element_part)
                     self._cursor += _local_idx
                     _local_idx = 0
                     return self._create_array(parts)
                 elif rune == '}':
+                    # If rune have '}' in array - set error
                     self._error("Unexpected end of array")
-                # If find new object or array
                 elif rune == '{' or rune == '[':
+                    # If find new object or array
+                    # Set new self._cursor += _local_idx, and set _local_idx
+                    # After recursion add new part
                     self._cursor += _local_idx
                     _local_idx = 0
                     _element_part = self.next()
                     parts.append(_element_part)
                     # If error inside then go out from recursion
-                    if self._status_error:
+                    if self._error_status:
                         return list()
             # If last rune is '{'
             elif _last_rune == '{':
                 if rune == ':':
+                    # If find ':' check comment.
+                    #   If we haven't comment -> add key and open context_dict to add value
+                    #   Else wait when find \n, then add key and open context_dict to add value
                     if _context_comment:
                         if _context_tab:
                             _element_part = self.text[_context_tab_index+1:(self._cursor+_local_idx)]
@@ -154,6 +198,9 @@ class LLMJSONToDict:
                         self._cursor += _local_idx
                         _local_idx = 0
                 elif rune == ',':
+                    # If find ',' check comment.
+                    #   If we have open context_dict -> add value
+                    #   Else next rune
                     if _context_value_dict:
                         _element_part = self.text[self._cursor+1:(self._cursor+_local_idx)]
                         if _element_part.strip() != '':
@@ -162,6 +209,9 @@ class LLMJSONToDict:
                         self._cursor += _local_idx
                         _local_idx = 0
                 elif rune == '}':
+                    # If find '}' check comment.
+                    #   If we have open context_dict -> add value
+                    #   Else return dict
                     if _context_value_dict:
                         _element_part = self.text[self._cursor+1:(self._cursor+_local_idx)]
                         parts.append(_element_part)
@@ -170,11 +220,15 @@ class LLMJSONToDict:
 
                     return self._create_dict(parts)
                 elif rune == ']':
+                    # If rune have ']' in array - set error
                     if not _context_comment:
                         self._error("Unexpected end of object")
                         return list()
                 # If find new object or array
                 elif rune == '[':
+                    # If find new object or array
+                    # Set new self._cursor += _local_idx, and set _local_idx
+                    # After recursion add new part
                     if _context_value_dict:
                         self._cursor += _local_idx
                         _local_idx = 0
@@ -182,7 +236,7 @@ class LLMJSONToDict:
                         parts.append(_element_part)
                         _context_value_dict = False
                         # If error inside then go out from recursion
-                        if self._status_error:
+                        if self._error_status:
                             return list()
                     else:
                         if not _context_comment:
@@ -210,13 +264,13 @@ class LLMJSONToDict:
         return parts
 
     def custom_load(self, json: str) -> (list, str):
-        self._cursor = 0
-        self.text = json
-        self._status_error = False
-        self._status_info = ""
+        self.new_text(json)
+        result, error = self.next(), self._error_info
+        if type(result) == list:
+            return result, error
+        else:
+            return list(result), error
 
-        return self.next(), self._status_info
-
-test_data = "[{'some': [[1,,,,,,],2,3,4,5], // [this] this works [, is comment: yep, yep \n other: 'text'}, {k: 1}]"
+test_data = "{'some': [[1, 'sone'],2,3,4,5], // [this] this works [, is comment: yep, yep \n other: 'text'}"
 worker = LLMJSONToDict()
 print(worker.custom_load(test_data))
